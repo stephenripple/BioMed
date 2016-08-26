4926883656b7b078f0de3c826ed1499118b19017
stephenripple <stephenripple@gmail.com>
2016-08-26 17:08:48 -0400

    Init2

---
+++ BioMed.Parsers.UnitTests/ExcelParserTests.cs
@@ -0,0 +1,28 @@
+﻿using BioMed.Parsers;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BioMed.Parsers.Tests
+{
+    [TestClass()]
+    public class ExcelParserTests
+    {
+        [TestMethod()]
+        public void LoadFile()
+        {
+            var excelparserclient = new BioMed.Parsers.ExcelParser(@"C:\BioMed\Patient Monitors - Examples. V1.1.xlsx");
+            var output = excelparserclient.toXML();
+            if (string.IsNullOrEmpty(output))
+                Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void TabToListTest()
+        {
+            var excelparserclient = new BioMed.Parsers.ExcelParser(@"C:\BioMed\Patient Monitors - Examples. V1.1.xlsx");
+            var output = excelparserclient.TabToList("ECG");
+            if (output == null)
+                Assert.Fail();
+        }
+    }
+}
---
+++ BioMed.Parsers/ExcelParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using OfficeOpenXml;
+using System.IO;
+
+namespace BioMed.Parsers
+{
+    public class ExcelParser
+    {
+        private FileInfo ExcelFileInfo;
+
+        public ExcelParser(string FilePath)
+        {
+            //Add validation for file
+            ExcelFileInfo = new FileInfo(FilePath);
+        }
+
+        public ExcelParser(FileInfo File)
+        {
+            ExcelFileInfo = File;
+        }
+        public string toXML()
+        {
+            using (var excelPackage = new ExcelPackage(ExcelFileInfo))
+            {
+                foreach (var worksheet in excelPackage.Workbook.Worksheets)
+                {
+                    var xml = worksheet.Cells.Text;
+                    return xml.ToString();
+                }
+
+            }
+            return string.Empty;
+        }
+
+        public List<string> TabToList(string tab)
+        {
+            using (var excelPackage = new ExcelPackage(ExcelFileInfo))
+            {
+                var worksheet = excelPackage.Workbook.Worksheets[tab];
+                var columncount = worksheet.Dimension.Columns;
+                var rowcount = worksheet.Dimension.Rows;
+            }
+            return null;
+        }
+    }    
+}
